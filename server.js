74b0b42e0c6e775f93f85b3a9ba7b3a80b472874
wingchunna <linktink123@gmail.com>
2023-04-14 11:42:27 +0700

    fix https to http

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const https = require("https");
+const http = require("http");
 const app = require("./App/app");
 const fs = require("fs");
 
 require("dotenv").config();
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
-const server = https.createServer(
+const server = http.createServer(
   // {
   //   key: fs.readFileSync("key.pem"),
   //   cert: fs.readFileSync("cert.pem"),
